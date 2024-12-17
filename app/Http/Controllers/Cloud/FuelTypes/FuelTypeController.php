a8915251211027382cdb1faa2cc06da9b9fb05ec
soelinaungdet <soelinaung.det@gmail.com>
2024-12-17 15:45:11 +0630

    fuel_type crud added

---
+++ app/Http/Controllers/Cloud/FuelTypes/FuelTypeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Cloud\FuelTypes;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Cloud\FuelTypes\CreateRequest;
+use App\Http\Requests\Cloud\FuelTypes\UpdateRequest;
+use App\Repositories\Cloud\Contracts\FuelTypes\FuelTypeRepositoryInterface;
+
+class FuelTypeController extends Controller
+{
+    private $fuelTypeRepository;
+    public function __construct(FuelTypeRepositoryInterface $fuelTypeRepository)
+    {
+        $this->fuelTypeRepository = $fuelTypeRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return $this->fuelTypeRepository->getFuelType($request);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateRequest $request)
+    {
+        return $this->fuelTypeRepository->createFuelType($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return $this->fuelTypeRepository->getDetailFuelType($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, $id)
+    {
+        return $this->fuelTypeRepository->updateFuelType($id, $request->validated());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        return $this->fuelTypeRepository->deleteFuelType($id);
+    }
+}
---
+++ app/Repositories/Cloud/Concretes/FuelTypes/FuelTypeRepository.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Repositories\Cloud\Concretes\FuelTypes;
+
+use App\Http\Resources\Cloud\FuelTypes\FuelTypeResource;
+use Exception;
+use App\Models\Station;
+use App\Traits\HasImage;
+use App\Traits\HasResponse;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\FuelType;
+use App\Repositories\Cloud\Contracts\FuelTypes\FuelTypeRepositoryInterface;
+
+class FuelTypeRepository implements FuelTypeRepositoryInterface
+{
+     use HasImage;
+     use HasResponse;
+     public function getFuelType($request)
+     {
+         $fuel_types = FuelType::paginate(10);
+
+         return FuelTypeResource::collection($fuel_types);
+     }
+
+     public function getDetailFuelType($id)
+     {
+         $fuel_type = FuelType::find($id);
+
+         if(!$fuel_type) {
+             return $this->errorResponse('Fuel Type not found', 404, null);
+         }
+
+         return new FuelTypeResource($fuel_type);
+     }
+
+     public function createFuelType($data)
+     {
+          try {
+
+                // Create a new fuel_type
+                $fuel_type = FuelType::create($data);
+
+                return new FuelTypeResource($fuel_type);
+
+          } catch(Exception $e) {
+              return $this->errorResponse($e->getMessage(), 500,  null);;
+          }
+     }
+
+     public function updateFuelType($id, $data)
+     {
+
+         // find the fuel_type by id
+         $fuel_type = FuelType::find($id);
+
+         // if the fuel_type doesn't exist, return an error response
+         if(!$fuel_type) {
+            return $this->errorResponse('Fuel Type not found', 404, null);
+         }
+
+         // update the fuel_type
+         $fuel_type->update($data);
+
+         return new FuelTypeResource($fuel_type);
+     }
+
+     public function deleteFuelType($id)
+     {
+        // find the fuel_type by id
+        $fuel_type = FuelType::find($id);
+
+        // if the fuel_type doesn't exist, return an error response
+        if(!$fuel_type) {
+            return $this->errorResponse('Fuel Type not found', 404, null);
+        }
+
+         // Delete the fuel_type's database
+         $fuel_type->delete();
+
+         return $this->successResponse('Fuel Type deleted successfully', 200, null);
+     }
+
+
+}
