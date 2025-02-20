5c77d619f509532a1896fdd95da60b1dc0304c3b
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2025-02-20 17:21:35 +0630

    mqtt client package hakhant/mqtt-client v1.0.0

---
+++ app/Console/Commands/Local/ListenMessage.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands\Local;
+
+use Hakhant\Broker\Client;
+use Illuminate\Console\Command;
+
+class ListenMessage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'listen:message';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Listen for mqtt topic';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $client = new Client(config('mqtt'));
+
+        $client->subscribe('detpos/#', function ($topic, $message) {
+            $this->info('Topic: ' . $topic);
+            $this->info('Message: ' . $message);
+
+            // Do something with the message here ( Queue Job )
+        });
+
+        $this->info('Successfully subscribed to topic detpos/#');
+
+        $client->loop();
+    }
+}
