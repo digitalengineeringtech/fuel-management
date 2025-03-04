d7ab77b466c273e66393013bec148238a7eac44f
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2025-03-04 13:30:38 +0630

    add dispenser and nozzle concrete implementation added

--- app/Repositories/Local/Concretes/Dispensers/DispenserRepository.php
+++ app/Repositories/Local/Concretes/Dispensers/DispenserRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repositories\Local\Concretes\Dispensers;
 
-use App\Repositories\Local\Contracts\Dispensers\DispenserRepositoryInterface;
+use Exception;
+use App\Models\Dispenser;
 use App\Traits\HasResponse;
+use App\Http\Resources\Local\Dispensers\DispenserResource;
+use App\Repositories\Local\Contracts\Dispensers\DispenserRepositoryInterface;
 
 class DispenserRepository implements DispenserRepositoryInterface
 {
@@ -11,26 +14,74 @@ class DispenserRepository implements DispenserRepositoryInterface
 
     public function getDispensers($request)
     {
-        //
+        try {
+            $dispensers = Dispenser::paginate(10);
+
+            return DispenserResource::collection($dispensers);
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function getDispenser($id)
     {
-        //
+        try {
+            $dispenser = Dispenser::find($id);
+
+            return new DispenserResource($dispenser);
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function createDispenser($data)
     {
-        //
+        try {
+            $dispenser = Dispenser::create($data);
+
+            if(!$dispenser) {
+                return $this->errorResponse('Failed to create dispenser', 400, null);
+            }
+
+            $this->successResponse('Dispenser created successfully', 201, new DispenserResource($dispenser));
+
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
+
     }
 
     public function updateDispenser($id, $data)
     {
-        //
+        try {
+            $dispenser = Dispenser::find($id);
+
+            if(!$dispenser) {
+                return $this->errorResponse('Dispenser not found', 404, null);
+            }
+
+            $dispenser->update($data);
+
+            return $this->successResponse('Dispenser updated successfully', 200, new DispenserResource($dispenser));
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function deleteDispenser($id)
     {
-        //
+        try {
+            $dispenser = Dispenser::find($id);
+
+            if(!$dispenser) {
+                return $this->errorResponse('Dispenser not found', 404, null);
+            }
+
+            $dispenser->delete();
+
+            return $this->successResponse('Dispenser deleted successfully', 200, null);
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 }
--- app/Repositories/Local/Concretes/Nozzles/NozzleRepository.php
+++ app/Repositories/Local/Concretes/Nozzles/NozzleRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories\Local\Concretes\Nozzles;
 
+use App\Http\Resources\Local\Nozzles\NozzleResource;
+use App\Models\Nozzle;
 use App\Traits\HasResponse;
 use App\Repositories\Local\Contracts\Nozzles\NozzleRepositoryInterface;
 
@@ -11,26 +13,74 @@ class NozzleRepository implements NozzleRepositoryInterface
 
     public function getNozzles($request)
     {
+        try {
+            $nozzles = Nozzle::paginate(10);
 
+            return NozzleResource::collection($nozzles);
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function getNozzle($id)
     {
+        try {
+            $nozzle = Nozzle::find($id);
 
+            return new NozzleResource($nozzle);
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function createNozzle($data)
     {
+        try {
+            $nozzle = Nozzle::create($data);
 
+            if(!$nozzle) {
+                return $this->errorResponse('Failed to create nozzle', 400, null);
+            }
+
+            return $this->successResponse('Nozzle created successfully', 201, new NozzleResource($nozzle));
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function updateNozzle($id, $data)
     {
+        try {
+            $nozzle = Nozzle::find($id);
 
+            if(!$nozzle) {
+                return $this->errorResponse('Nozzle not found', 404, null);
+            }
+
+            $nozzle->update($data);
+
+            return $this->successResponse('Nozzle updated successfully', 200, new NozzleResource($nozzle));
+
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 
     public function deleteNozzle($id)
     {
+        try {
+            $nozzle = Nozzle::find($id);
 
+            if(!$nozzle) {
+                return $this->errorResponse('Nozzle not found', 404, null);
+            }
+
+            $nozzle->delete();
+
+            return $this->successResponse('Nozzle deleted successfully', 200, null);
+
+        } catch(Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500, null);
+        }
     }
 }
