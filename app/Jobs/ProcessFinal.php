50f09f91c8e906cc6a47099afb424c8192db2b92
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2025-03-18 13:22:06 +0630

    dispenser seeder and job modify

--- app/Jobs/ProcessFinal.php
+++ app/Jobs/ProcessFinal.php
@@ -44,5 +44,7 @@ class ProcessFinal
          $updatedSale = $this->updateSale($cachedSale, $this->messages);
 
          $this->client->publish('detpos/local_server/'.$nozzle->dispenser_id, $nozzle->nozzle_no.'D1S1');
+
+         $this->client->disconnect();
     }
 }
